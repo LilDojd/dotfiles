c5069debb423e850ecb37a5ac645f0f1e0007251
Georgy Andreev <g.andreev@insilicomedicine.com>
2024-05-24 23:08:41 +0400

    Switch to packrust

--- .config/nvim/lua/plugins/pack-rust.lua
+++ .config/nvim/lua/plugins/pack-rust.lua
@@ -1,173 +1,173 @@
--- return {
---   { import = "astrocommunity.pack.rust" },
--- }
 return {
-  { import = "astrocommunity.pack.toml" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "rust" })
-      end
-    end,
-  },
-  {
-    "AstroNvim/astrolsp",
-    dependencies = {
-      "lvimuser/lsp-inlayhints.nvim",
-      opts = {
-        inlay_hints = {
-          parameter_hints = {
-            show = true,
-            prefix = "<- ",
-            separator = ", ",
-            remove_colon_start = false,
-            remove_colon_end = true,
-          },
-          type_hints = {
-            -- type and other hints
-            show = true,
-            prefix = "-> ",
-            separator = ", ",
-            remove_colon_start = true,
-            remove_colon_end = false,
-          },
-          only_current_line = false,
-          -- separator between types and parameter hints. Note that type hints are
-          -- shown before parameter
-          labels_separator = "  ",
-          -- whether to align to the length of the longest line in the file
-          max_len_align = false,
-          -- padding from the left if max_len_align is true
-          max_len_align_padding = 1,
-          -- highlight group
-          highlight = "LspInlayHint",
-          -- virt_text priority
-          priority = 0,
-        },
-        enabled_at_startup = true,
-        debug_mode = false,
-      },
-    },
-    optional = true,
-    ---@param opts AstroLSPOpts
-    opts = {
-      handlers = { rust_analyzer = false }, -- disable setup of `rust_analyzer`
-      ---@diagnostic disable: missing-fields
-      config = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              checkOnSave = {
-                command = "clippy",
-              },
-              assist = {
-                importEnforceGranularity = true,
-                importPrefix = "crate",
-              },
-              completion = {
-                postfix = {
-                  enable = false,
-                },
-              },
-              inlayHints = {
-                lifetimeElisionHints = {
-                  enable = true,
-                  useParameterNames = true,
-                },
-              },
-            },
-          },
-        },
-      },
-      on_attach = function(client, bufnr) require("lsp-inlayhints").on_attach(client, bufnr) end,
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
-    end,
-  },
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    ft = "rust",
-    opts = function()
-      local adapter
-      local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
-      local cfg = require "rustaceanvim.config"
-      if success then
-        local package_path = package:get_install_path()
-        local codelldb_path = package_path .. "/codelldb"
-        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
-        local this_os = vim.loop.os_uname().sysname
-
-        -- The path in windows is different
-        if this_os:find "Windows" then
-          codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
-          liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
-        else
-          -- The liblldb extension is .so for linux and .dylib for macOS
-          liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-        end
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
-      else
-        adapter = cfg.get_codelldb_adapter()
-      end
-
-      local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
-      return { server = astrolsp_avail and astrolsp.lsp_opts "rust_analyzer", dap = { adapter = adapter } }
-    end,
-    config = function(_, opts) vim.g.rustaceanvim = require("astrocore").extend_tbl(opts, vim.g.rustaceanvim) end,
-  },
-  {
-    "Saecki/crates.nvim",
-    lazy = true,
-    dependencies = {
-      "AstroNvim/astrocore",
-      opts = {
-        autocmds = {
-          CmpSourceCargo = {
-            {
-              event = "BufRead",
-              desc = "Load crates.nvim into Cargo buffers",
-              pattern = "Cargo.toml",
-              callback = function()
-                require("cmp").setup.buffer { sources = { { name = "crates" } } }
-                require "crates"
-              end,
-            },
-          },
-        },
-      },
-    },
-    opts = {
-      src = {
-        cmp = { enabled = true },
-      },
-      null_ls = {
-        enabled = true,
-        name = "crates.nvim",
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    opts = function(_, opts)
-      if not opts.adapters then opts.adapters = {} end
-      local rustaceanvim_avail, rustaceanvim = pcall(require, "rustaceanvim.neotest")
-      if rustaceanvim_avail then table.insert(opts.adapters, rustaceanvim) end
-    end,
-  },
+  { import = "astrocommunity.pack.rust" },
 }
+-- return {
+--   { import = "astrocommunity.pack.toml" },
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     optional = true,
+--     opts = function(_, opts)
+--       if opts.ensure_installed ~= "all" then
+--         opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "rust" })
+--       end
+--     end,
+--   },
+--   {
+--     "AstroNvim/astrolsp",
+--     dependencies = {
+--       "lvimuser/lsp-inlayhints.nvim",
+--       opts = {
+--         inlay_hints = {
+--           parameter_hints = {
+--             show = true,
+--             prefix = "<- ",
+--             separator = ", ",
+--             remove_colon_start = false,
+--             remove_colon_end = true,
+--           },
+--           type_hints = {
+--             -- type and other hints
+--             show = true,
+--             prefix = "-> ",
+--             separator = ", ",
+--             remove_colon_start = true,
+--             remove_colon_end = false,
+--           },
+--           only_current_line = false,
+--           -- separator between types and parameter hints. Note that type hints are
+--           -- shown before parameter
+--           labels_separator = "  ",
+--           -- whether to align to the length of the longest line in the file
+--           max_len_align = false,
+--           -- padding from the left if max_len_align is true
+--           max_len_align_padding = 1,
+--           -- highlight group
+--           highlight = "LspInlayHint",
+--           -- virt_text priority
+--           priority = 0,
+--         },
+--         enabled_at_startup = true,
+--         debug_mode = false,
+--       },
+--     },
+--     optional = true,
+--     ---@param opts AstroLSPOpts
+--     opts = {
+--       handlers = { rust_analyzer = false }, -- disable setup of `rust_analyzer`
+--       ---@diagnostic disable: missing-fields
+--       config = {
+--         rust_analyzer = {
+--           settings = {
+--             ["rust-analyzer"] = {
+--               checkOnSave = {
+--                 command = "clippy",
+--               },
+--               assist = {
+--                 importEnforceGranularity = true,
+--                 importPrefix = "crate",
+--               },
+--               completion = {
+--                 postfix = {
+--                   enable = false,
+--                 },
+--               },
+--               inlayHints = {
+--                 lifetimeElisionHints = {
+--                   enable = true,
+--                   useParameterNames = true,
+--                 },
+--               },
+--             },
+--           },
+--         },
+--       },
+--       on_attach = function(client, bufnr) require("lsp-inlayhints").on_attach(client, bufnr) end,
+--     },
+--   },
+--   {
+--     "jay-babu/mason-nvim-dap.nvim",
+--     optional = true,
+--     opts = function(_, opts)
+--       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+--     end,
+--   },
+--   {
+--     "WhoIsSethDaniel/mason-tool-installer.nvim",
+--     optional = true,
+--     opts = function(_, opts)
+--       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+--     end,
+--   },
+--   {
+--     "mrcjkb/rustaceanvim",
+--     version = "^4",
+--     ft = "rust",
+--     opts = function()
+--       local adapter
+--       local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
+--       local cfg = require "rustaceanvim.config"
+--       if success then
+--         local package_path = package:get_install_path()
+--         local codelldb_path = package_path .. "/codelldb"
+--         local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+--         local this_os = vim.loop.os_uname().sysname
+--
+--         -- The path in windows is different
+--         if this_os:find "Windows" then
+--           codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+--           liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+--         else
+--           -- The liblldb extension is .so for linux and .dylib for macOS
+--           liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+--         end
+--         adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+--       else
+--         adapter = cfg.get_codelldb_adapter()
+--       end
+--
+--       local astrolsp_avail, astrolsp = pcall(require, "astrolsp")
+--       return { server = astrolsp_avail and astrolsp.lsp_opts "rust_analyzer", dap = { adapter = adapter } }
+--     end,
+--     config = function(_, opts) vim.g.rustaceanvim = require("astrocore").extend_tbl(opts, vim.g.rustaceanvim) end,
+--   },
+--   {
+--     "Saecki/crates.nvim",
+--     lazy = true,
+--     dependencies = {
+--       "AstroNvim/astrocore",
+--       opts = {
+--         autocmds = {
+--           CmpSourceCargo = {
+--             {
+--               event = "BufRead",
+--               desc = "Load crates.nvim into Cargo buffers",
+--               pattern = "Cargo.toml",
+--               callback = function()
+--                 require("cmp").setup.buffer { sources = { { name = "crates" } } }
+--                 require "crates"
+--               end,
+--             },
+--           },
+--         },
+--       },
+--     },
+--     opts = {
+--       src = {
+--         cmp = { enabled = true },
+--       },
+--       null_ls = {
+--         enabled = true,
+--         name = "crates.nvim",
+--       },
+--     },
+--   },
+--   {
+--     "nvim-neotest/neotest",
+--     optional = true,
+--     opts = function(_, opts)
+--       if not opts.adapters then opts.adapters = {} end
+--       local rustaceanvim_avail, rustaceanvim = pcall(require, "rustaceanvim.neotest")
+--       if rustaceanvim_avail then table.insert(opts.adapters, rustaceanvim) end
+--     end,
+--   },
+-- }
