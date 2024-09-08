fdf6a973678a4a24ef63ccb5cb389f4620856e62
Georgy Andreev <yawner@pm.me>
2024-09-08 12:47:13 +0400

    :sparkles:(nvim) move plugins around

--- .config/nvim/lua/plugins/md-preview.lua
+++ .config/nvim/lua/plugins/md-preview.lua
@@ -1,7 +1,16 @@
 return {
   {
-    "henriklovhaug/Preview.nvim",
-    cmd = { "Preview" },
-    config = function() require("preview").setup() end,
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
   },
 }
