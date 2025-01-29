4ab850de7412c4b053c879bb69ee980103040c44
LilDojd <yawner@protonmail.com>
2025-01-29 15:03:45 +0400

    ✨ feat: enhance neovim configuration with new plugins and improvements
    The commit message focuses on the main theme of the changes, which is enhancing
    the Neovim setup with new plugins and configuration improvements, including:
    - Update leetcode plugin with better startup and language support
    - Add WGSL language support
    - Add toggleterm for terminal management
    - Add typr for typing practice
    - Update obsidian templates path

--- .config/nvim/lua/plugins/leetcode.lua
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -1,18 +1,55 @@
+---@type LazySpec
 return {
   "kawre/leetcode.nvim",
-  build = ":TSUpdate html",
+  cmd = "Leet",
   dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim", -- required by telescope
-    "MunifTanjim/nui.nvim",
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-lua/plenary.nvim" }, -- required by telescope
+    { "MunifTanjim/nui.nvim" },
 
     -- optional
-    "nvim-treesitter/nvim-treesitter",
-    "rcarriga/nvim-notify",
-    "nvim-tree/nvim-web-devicons",
+    { "rcarriga/nvim-notify", optional = true },
+    { "nvim-tree/nvim-web-devicons", optional = true },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      optional = true,
+      opts = function(_, opts)
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "html" })
+        end
+      end,
+    },
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        autocmds = {
+          leetcode_autostart = {
+            {
+              event = "VimEnter",
+              desc = "Start leetcode.nvim on startup",
+              nested = true,
+              callback = function()
+                if vim.fn.argc() ~= 1 then return end -- return if more than one argument given
+                local arg = vim.tbl_get(require("astrocore").plugin_opts "leetcode.nvim", "arg") or "leetcode.nvim"
+                if vim.fn.argv()[1] ~= arg then return end -- return if argument doesn't match trigger
+                local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+                if #lines > 1 or (#lines == 1 and lines[1]:len() > 0) then return end -- return if buffer is non-empty
+                require("leetcode").start(true)
+              end,
+            },
+          },
+        },
+      },
+    },
   },
   opts = {
-    lang = "rust",
+    lang = "python3",
+    injector = { ---@type table<lc.lang, lc.inject>
+      ["python3"] = {
+        before = true,
+      },
+    },
     hooks = {
       ---@type fun(question: lc.ui.Question)[]
       ["question_enter"] = {
@@ -28,7 +65,6 @@ return {
             else
               print("Failed update rust-project.json. Error: " .. error_message)
             end
-            vim.cmd "SupermavenStop"
           end
         end,
       },
--- .config/nvim/lua/plugins/md-preview.lua
+++ .config/nvim/lua/plugins/md-preview.lua
@@ -1,7 +1,7 @@
 return {
   -- {
-  --   "OXY2DEV/markview.nvim",
-  --   lazy = false, -- Recommended
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
   --   -- ft = "markdown" -- If you decide to lazy-load anyway
   --
   --   dependencies = {
