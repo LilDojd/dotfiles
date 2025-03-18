75473a2bf87fe15f7076ec8ba13a7565d8759bbb
LilDojd <yawner@protonmail.com>
2025-03-18 17:58:57 +0400

    Fix typos

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -1,6 +1,6 @@
 abbr -a mirrors "sudo reflector --verbose --latest 5 --country 'United Arab Emirates' --age 6 --sort rate --save /etc/pacman.d/mirrorlist"
 abbr -a fm yazi
 abbr -a ya yazi
-abbr -a yays "yay -Slq | fzf -m --preview 'cat <(yay -Si {1}) <(yay -Fl {1} | awk \"{print \$2}\")' | xargs -ro yay -S"
+abbr -a yays "yay -Slq | fzf -m --preview 'cat (yay -Si {1} | psub) (yay -Fl {1} | awk \"{print \$2}\" | psub)' | xargs -ro yay -S"
 abbr -a pacr "pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns"
 abbr -a p "pacman -Q | fzf"
